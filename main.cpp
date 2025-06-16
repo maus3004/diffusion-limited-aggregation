a5402ea315a8ac290aaff0ed81dfff9303b1d241
maus3004 <mattbaek3@gmail.com>
2025-06-15 18:43:43 -0700

    updated code

--- main.cpp
+++ main.cpp
@@ -1,40 +1,56 @@
 #include <iostream>
 #include <cstdlib>
+#include <random>
 #include <vector>
 using namespace std;
 
 
 
 // void search
-void walk(vector<vector<int>> coordinates, int cube_length, int length_x, int length_y){
+int walk(vector<vector<int>> coordinates, int cube_length, int length){
     // add and delete a coordinate until we find it next to cube
     // but we can't just randomly add a random point until it's next to another point, that wouldn't be walking
     // possibly use a pointer and then make a new pointer that's a cube_length away and then delete the first pointer
     // check the second pointer
 
-    int row = rand(0, length_x), col = rand(0, length_y);
+    random_device rd;
+    uniform_int_distribution<int> dist(0, length);
+    int init_x = dist(rd), init_y = dist(rd);
 
+    coordinates[init_x][init_y] = 1;
+
+    // if somehow it lands on the initial box, rerun it
+    if(init_x == length / 2 || init_y == length / 2){
+        walk(coordinates, cube_length, length);
+    }
+
+    // the real search algorithm
+
+    return init_x, init_y;
 
 }
 
 int main() {
-    int user_seed;
-    cout << "Enter seed: ";
-    cin >> user_seed;
-    srand(user_seed);
-
     // using a coordinate system
-    int start_dimension_x = 10, start_dimension_y = 10;
+    int start_dimension = 10;
     int cube_length = 1;
 
-    int threshold = (start_dimension_x * start_dimension_y) / 7;
+    int threshold = (start_dimension ^ 2) / 7;
 
-    // begin with a
+    // initialize an empty board
     vector<vector<int>> coordinates = {};
+    for(int rows = 0; rows < start_dimension; rows++){
+        for(int cols = 0; cols < rows; cols++){
+            coordinates[rows][cols] = 0;
+        }
+    }
+
+    // putting a "block" on
+    coordinates[start_dimension/2][start_dimension/2] = 1;
 
     // the meat of the algorithm is here
     for(int i = 0; i < threshold; i++){
-        walk(coordinates, cube_length, start_dimension_x, start_dimension_y);
+        walk(coordinates, cube_length, start_dimension);
     }
 
     for(const auto& point : coordinates){
